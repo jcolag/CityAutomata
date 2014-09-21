275ebbffa5bb4eba0bf9f7c2ca8a32501a022a3b
John Colagioia <john@colagioia.net>
2014-09-21 09:18:07 -0400

    Basic skeleton

---
+++ CityAutomata/Block.cs
@@ -0,0 +1,53 @@
+﻿namespace CityAutomata
+{
+        using System;
+
+        public class Block
+        {
+                private readonly int capacity;
+
+                private int level = 1;
+
+                private int population = 0;
+
+                private float value = 0;
+
+                private int cost = 0;
+
+                private int collected = 0;
+
+                public Block(int capacity)
+                {
+                        this.capacity = capacity;
+                }
+
+                public int Population
+                {
+                        get
+                        {
+                                return this.population;
+                        }
+                }
+
+                public float Value
+                {
+                        get
+                        {
+                                return this.value;
+                        }
+
+                        set
+                        {
+                                this.value = value;
+                        }
+                }
+
+                public int Cost
+                {
+                        get
+                        {
+                                return cost;
+                        }
+                }
+        }
+}
---
+++ CityAutomata/City.cs
@@ -0,0 +1,47 @@
+﻿namespace CityAutomata
+{
+        using System;
+
+        public class City
+        {
+                private readonly Block[][] grid;
+
+                public City(int x, int y)
+                {
+                        grid = new Block[x][y];
+                }
+
+                public void ScanAll()
+                {
+                        for (int i = 0; i < grid.GetLength(0); i++)
+                        {
+                                for (int j = 0; j < grid.GetLength(1); j++)
+                                {
+                                        grid[i][j].Value = CalculateValue(i, j);
+                                }
+                        }
+                }
+
+                private float CalculateValue(int x, int y)
+                {
+                        float total = 0.0;
+
+                        for (int i = 0; i < grid.GetLength(0); i++)
+                        {
+                                for (int j = 0; j < grid.GetLength(1); j++)
+                                {
+                                        if (i == x && j == y)
+                                        {
+                                                continue;
+                                        }
+                                        int xdist = x - i;
+                                        int ydist = y - j;
+                                        double dist = Math.Sqrt((double)(xdist * xdist + ydist * ydist));
+                                        total += (float)grid[i][j].Population / dist;
+                                }
+                        }
+
+                        return total;
+                }
+        }
+}
