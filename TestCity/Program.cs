5da575a9be1366564442c4333f483f34a8fc755a
John Colagioia <john@colagioia.net>
2014-09-22 06:35:43 -0400

    Main program

---
+++ TestCity/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TestCity
+{
+        using System;
+        using CityAutomata;
+
+        class MainClass
+        {
+                public static void Main(string[] args)
+                {
+                        var city = new City(10, 10);
+
+                        city.ScanAll();
+                }
+        }
+}
